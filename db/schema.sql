0b85d33f508e1b1213ae7f0135e7d32f27ccf722
Annaruth McBride <armcbride17@gmail.com>
2020-04-29 18:08:07 -0500

    adding manager id

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,10 @@ rol_id INT AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(10,4),
 dep_id INT,
+man_id INT,
 PRIMARY KEY (rol_id),
-FOREIGN KEY(dep_id) REFERENCES department(dep_id) 
+FOREIGN KEY(dep_id) REFERENCES department(dep_id),
+FOREIGN KEY(man_id) REFERENCES employee(man_id) 
 );
 
 CREATE TABLE employee ( 
@@ -23,8 +25,10 @@ emp_id INT AUTO_INCREMENT NOT NULL,
 first_name varchar(30),
 last_name varchar(30),
 rol_id INT,
+man_id INT,
 PRIMARY KEY (emp_id),
 FOREIGN KEY (rol_id) REFERENCES role(rol_id)
+FOREIGN KEY (man_id) REFERENCES role(man_id)
 );
 
 
@@ -40,15 +44,15 @@ INSERT INTO role
 VALUES
     ('Sales Lead', 100000, 1),
     ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
+    ('Lead Engineer', 150000, 2, ),
     ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
+    ('Account Manager', 160000, 3, ),
     ('Accountant', 125000, 3),
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
     
 INSERT INTO employee
-    (first_name, last_name, rol_id)
+    (first_name, last_name, rol_id, man)
 VALUES
     ('John', 'Doe', 1),
     ('Mike', 'Chan', 2, 1),
