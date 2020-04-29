2d51093d10665c35fa4c1178fb74908e5c157757
Annaruth McBride <armcbride17@gmail.com>
2020-04-28 22:11:46 -0500

    adjusted

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const util = require("util");
 const mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Aa6844598",
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
     database: "emp_tracker"
   });
   
